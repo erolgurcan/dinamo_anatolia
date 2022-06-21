3bea020140b1f81516620440f0b89e9adb208fce
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-06-20 22:59:52 -0700

    Update InterFaceNav.js

--- src/InterFaceNav.js
+++ src/InterFaceNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Fragment } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Login from "./component/Login/Login";
 import LandingPage from "./component/LandingPage/LandingPage";
 import UserDashBoard from "./component/UserDashBoard/UserDashBoard";
@@ -8,13 +8,13 @@ import UserDashBoard from "./component/UserDashBoard/UserDashBoard";
 const InterFaceNav = () => {
   return (
     <Fragment>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route exact path={"/"} element={<LandingPage />} />
+          <Route path={"/"} element={<LandingPage />} />
           <Route path={"/login"} element={<Login />} />
           <Route path={"/dashboard/*"} element={<UserDashBoard />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Fragment>
   );
 };
