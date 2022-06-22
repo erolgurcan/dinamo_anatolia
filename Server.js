b9eb4c02883797c2dc5f82f8a20e8d43804d5a33
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-06-21 17:22:01 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,10 +6,29 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "build")));
 
+//DB Settings
+const { Client } = require("pg");
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+client.connect();
+
 app.listen(PORT, () => {
-  console.log("Aplication started on port " + PORT);
+    console.log("Aplication started on port " + PORT);
+  });
+  
+app.get("/test", (req, res) => {
+  try {
+      client.query("select * from test", (err, response) => {
+      res.json(response.rows);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
