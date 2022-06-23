fb93551df8c6498b135a1a9715b7366f81652e21
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-06-23 00:03:42 -0700

    Client fix

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const path = require("path");
-
+const { Client } = require("pg");
 app.use(cors());
 app.use(express.json());
 
@@ -11,7 +11,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production ") {
   app.use(express.static(path.join(__dirname, "build")));
-  const { Client } = require("pg");
+
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
