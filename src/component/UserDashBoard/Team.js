47db8fe4237b768a10abe2eb32fb28877c20501d
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-07-15 11:20:48 -0700

    install re-charts

--- src/component/UserDashBoard/Team.js
+++ src/component/UserDashBoard/Team.js
@@ -137,26 +137,7 @@ const Team = () => {
 
                   <div class="card-body">
                     <div class="chart-area">
-                      <XYPlot width={300} height={300} stackBy="y" animation>
-                        <VerticalGridLines />
-                        <HorizontalGridLines />
-                        <XAxis />
-                        <YAxis />
-                        <VerticalBarSeries
-                          data={[
-                            { x: 2, y: 10 },
-                            { x: 4, y: 5 },
-                            { x: 5, y: 15 },
-                          ]}
-                        />
-                        <VerticalBarSeries
-                          data={[
-                            { x: 2, y: 12 },
-                            { x: 4, y: 2 },
-                            { x: 5, y: 11 },
-                          ]}
-                        />
-                      </XYPlot>
+   
                     </div>
                   </div>
                 </div>
