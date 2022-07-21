8177857b038b07be8d613892e7eef6d3141ddb02
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-07-21 15:12:01 -0700

    test client query

--- routes/jsonTokenAuth.js
+++ routes/jsonTokenAuth.js
@@ -3,26 +3,31 @@ const { Client } = require("pg");
 const bcrypt = require("bcrypt");
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-  
-  client.connect();
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
 
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log("select * from users where  user_email = " + "'" + email + "';");
+    console.log(
+      "select * from users where  user_email = " + "'" + email + "';"
+    );
+    client.query(
+      "select * from users where  user_email = " + "'" + email + "';",
+      (err, response) => {
+        res.json(response.rows);
+      }
+    );
 
     // client.query("select * from users where  user_email = " + "'" + email + "';" ,  (err, response) => {
     //     res.json(response.rows);
     //   });
-
-
   } catch (error) {}
 });
-
 
 module.exports = router;
