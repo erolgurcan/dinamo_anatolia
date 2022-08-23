8e9d67216263c23b9bc9c2d77a82dc218dd362f7
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-22 19:16:04 -0700

    connection String

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const path = require("path");
 const { Client } = require("pg");
-const { connectionString } = require("./db")
+const connectionString = require("../db") || process.env.DATABASE_URL ;
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
@@ -31,6 +31,7 @@ app.listen(PORT, () => {
 //Routes
 
 app.use("/auth", require("./routes/jsonTokenAuth"));
+app.use("/teamInfo", require("./routes/teamInfo"))
 
 //Api
 
